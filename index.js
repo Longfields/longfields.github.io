ea876c68d1b04fd420ad352a01733e7b7f56df59
warsturdy <75341047+warsturdy@users.noreply.github.com>
2023-03-26 17:51:35 -0400

    26/03/23

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
-import { createServer } from "http";
-import { Server } from "socket.io";
-
-
-
 const express = require('express');
 const app = express();
 const http = require('http');
-/*const server = http.createServer(app);
+const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);*/
-const httpServer = createServer();
-const io = new Server(httpServer, {
-  path: "/chat.html"
-});
+const io = new Server(server);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/chat.html');
